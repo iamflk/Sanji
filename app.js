45af042697b413ae2de1f3ed121bfc25bf12d4c0
CapiCode <devcapicode@gmail.com>
2021-01-10 21:02:50 +0100

    Add handler

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const { Client } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection } = require('discord.js');
 const { TOKEN, PREFIX } = require('./config.js');
 const client = new Client();
+client.commands = new Collection();
+
+const commandsFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+console.log(`${commandsFiles} 🔧`);
+
+for(const file of commandsFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+    console.log(`${client.commands}  🗃️`);
+    console.log(`Loading: ${command.name} 🔄`);
+}
 
 client.on('ready', () => {
-  console.log(`Sanji à votre service ${client.user.tag}!`);
+  console.log(`Sanji à votre service ${client.user.tag} 👨‍🍳`);
 });
 
-client.on('message', msg => {
-  if (msg.content === '/sanji') {
-    msg.reply('**S\'allume une clope**  🚬');
-  }
+client.on('message', message => {
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (!client.commands.has(command)) return;
+    client.commands.get(command).execute(client, message, args);
 });
 
 client.login(TOKEN);
---
+++ commands/sanji.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'sanji',
+    description: 'S\'en grille une',
+    execute(client, message, args) {
+        message.channel.send('**S\'allume une clope**  🚬');
+    }
+}
